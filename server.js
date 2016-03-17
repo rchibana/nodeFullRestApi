20e0108fa242800943f9347730b2e13b318d70af
Rodrigo Chibana <chibana_rodrigo@hotmail.com>
2016-03-17 11:34:00 -0300

    changed the server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/listUsers', function(req, res){
 
 });
 
-var server = app.listen(8081,funtion(){
+var server = app.listen(8081, function(){
 
 	var host = server.address().address
 	var port = server.address().port
